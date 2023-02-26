8213aa93000b1ff2dfd396cc54c2f1af5a238727
Paulo David <pdhc2212@gmail.com>
2023-02-25 21:18:48 -0500

    rutas

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def inicio():
     return 'Hola mundo desde Flask'
+
+@app.route('/saludar/<nombre>')
+def saludar(nombre):
+    return f'Hola {nombre}'
+
+@app.route('/edad/<int:edad>')
+def mostrar_edad(edad):
+    return f'Tu edad es: {edad}'
